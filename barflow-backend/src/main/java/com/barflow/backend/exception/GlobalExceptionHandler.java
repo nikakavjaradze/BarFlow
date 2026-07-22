cf5a9e8ce40cdc206d02d4af55f4c05cc0b5e628
nikakavjaradze <nikakavjaradze@gmail.com>
2026-07-23 03:39:11 +0400

    added Valid Exception

--- barflow-backend/src/main/java/com/barflow/backend/exception/GlobalExceptionHandler.java
+++ barflow-backend/src/main/java/com/barflow/backend/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.barflow.backend.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -13,5 +17,14 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleCocktailNotFound(CocktailNotFoundException ex) {
         return Map.of("message", ex.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleBlackField(MethodArgumentNotValidException ex) {
+        Map<String,String> errors = new HashMap<>();
+        for(FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(),error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors.toString());
     }
 }
